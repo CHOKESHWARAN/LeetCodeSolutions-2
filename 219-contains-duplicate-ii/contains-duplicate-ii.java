f64d36f77e46992edcc95137cac0ef6480723b43
CHOKESHWARAN <chokesh1906@gmail.com>
2026-09-15 18:25:19 +0530

    Time: 33 ms (20.79%) | Memory: 124.9 MB (9.50%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,6 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
