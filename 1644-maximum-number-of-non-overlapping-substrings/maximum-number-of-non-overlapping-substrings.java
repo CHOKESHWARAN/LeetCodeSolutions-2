97e755542519e5b1df8e3c2f574e3c727c635ffc
CHOKESHWARAN <chokesh1906@gmail.com>
2026-09-18 05:53:23 +0530

    Time: 8 ms (96.16%) | Memory: 47.9 MB (78.26%) - LeetSync

---
+++ 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<String> maxNumOfSubstrings(String s) {
+        int[] first = new int[26];
+        int[] last = new int[26];
+        Arrays.fill(first, -1);
+        
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i) - 'a';
+            if (first[c] == -1) first[c] = i;
+            last[c] = i;
+        }
+        
+        List<int[]> intervals = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            if (first[i] == -1) continue;
+            int l = first[i], r = last[i];
+            boolean valid = true;
+            
+            for (int j = l; j <= r; j++) {
+                int c = s.charAt(j) - 'a';
+                if (first[c] < l) {
+                    valid = false;
+                    break;
+                }
+                r = Math.max(r, last[c]);
+            }
+            
+            if (valid) {
+                intervals.add(new int[]{l, r});
+            }
+        }
+        
+        Collections.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        
+        List<String> ans = new ArrayList<>();
+        int prevEnd = -1;
+        for (int[] interval : intervals) {
+            if (interval[0] > prevEnd) {
+                ans.add(s.substring(interval[0], interval[1] + 1));
+                prevEnd = interval[1];
+            }
+        }
+        
+        return ans;
+    }
+}
